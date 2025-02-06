ed71045285c169c71252669bbd0be15fc5d09dfc
Yuhao David Xia <yuhao.xia@enti.cat>
2025-02-06 14:13:42 +0100

    Update

--- M03UF3_AA1_Yuhao_VictoriaSol/M03UF3_AA1_Yuhao_Victoria/main.cpp
+++ M03UF3_AA1_Yuhao_VictoriaSol/M03UF3_AA1_Yuhao_Victoria/main.cpp
@@ -9,17 +9,23 @@ int main() {
 	char table[COLUMNS][ROWS];
 	int playerInputX, playerInputY;
 
+	for (int i = 0; i < COLUMNS; i++) {
+		for (int j = 0; j < ROWS; j++) {
+			table[i][j] = ' ';
+		}
+	}
+
 	std::cout << "Ingrese una posicion valida (0-2): " << std::endl;
 	std::cin >> playerInputX >> playerInputY;
 
-	while (playerInputX < 0 || playerInputX > 2) {
+	while ((playerInputX < 0 || playerInputX > 2) && std::isdigit(playerInputX)) {
 
 		std::cout << "Posicion X invalido. Ingrese un numero entre el 0 y el 2" << std::endl;
 		std::cin >> playerInputX;
 
 	}
 
-	while (playerInputY < 0 || playerInputY > 2) {
+	while ((playerInputY < 0 || playerInputY > 2) && std::isdigit(playerInputY)) {
 
 		std::cout << "Posicion Y invalido. Ingrese un numero entre el 0 y el 2" << std::endl;
 		std::cin >> playerInputY;
