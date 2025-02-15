dbe775ec1aa6108f101e5d79f1b0ac9df1c3b243
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2025-02-15 23:56:38 +0100

    ahora si

--- M03UF3_AA1_Yuhao_VictoriaSol/M03UF3_AA1_Yuhao_Victoria/main.cpp
+++ M03UF3_AA1_Yuhao_VictoriaSol/M03UF3_AA1_Yuhao_Victoria/main.cpp
@@ -112,8 +112,8 @@ int main() {
         while (!ganar) {                        //Se repite este bucle hasta que se gane o se empate
             tableroImprimir(table, tableroVisual);
             elegirAccion(ganar, table, tableroVisual, repetir, playerInputX, playerInputY); //Lo mandamos al menú de elegir de si guardar partida, poner una ficha o ir al menu principal
-            if ((playerInputX < 0 || playerInputX > 2) || (playerInputY < 0 || playerInputY > 2)) {     //Comprobamos que no haya un número extremadamente bajo o grande
-                playerInputs(table, playerInputX, playerInputY);
+            while ((playerInputX < 0 || playerInputX > 2) || (playerInputY < 0 || playerInputY > 2)) {     //Comprobamos que no haya un número extremadamente bajo o grande
+                elegirAccion(ganar, table, tableroVisual, repetir, playerInputX, playerInputY);
             }
             if(!repetir)player(table, playerInputX, playerInputY);      //Si repetir esta en true entonces es que no hemos seleccionado una ficha y eso genera error
             winOrLose(table, ganar);    //Comprobar si gana o no
